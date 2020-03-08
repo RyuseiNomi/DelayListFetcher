e252aa7ec8014dc000d17ae1293b86e0841fb82d
RyuseiNomi <kanontonto1010@gmail.com>
2020-03-08 22:26:17 +0900

    fetch delay-list from web-site

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,9 +1,22 @@
 package handler
 
 import (
+	"io/ioutil"
 	"log"
+	"net/http"
+)
+
+var (
+	url = "https://tetsudo.rti-giken.jp/free/delay.json"
 )
 
 func Handler() {
-	log.Print("Hello World")
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Can not get delay list! Error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	log.Printf(string(byteArray))
 }
