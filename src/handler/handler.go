ebed2ff3d28a57f6157f86bd87ea36b631a2007f
RyuseiNomi <kanontonto1010@gmail.com>
2020-04-05 16:56:35 +0900

    exctact process to create json

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	jsonWorker "github.com/RyuseiNomi/delay_reporter_lm/src/json"
 	s3Uploader "github.com/RyuseiNomi/delay_reporter_lm/src/s3"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -22,7 +22,7 @@ var (
 func Handler() {
 	delayList := getDelayList()
 
-	if err := createJSON(delayList); err != nil {
+	if err := jsonWorker.CreateJSON(delayList); err != nil {
 		log.Fatal("Create JSON Error:  %+v\n", err)
 	}
 
@@ -61,38 +61,4 @@ func getDelayList() []byte {
 
 	log.Printf("Succeeded to get Delay-list!")
 	return delayList
-}
-
-/**
- * Create empty JSON file on temp file and write fetched delay-list
- */
-func createJSON(delayList []byte) error {
-	if delayList == nil {
-		return fmt.Errorf("create JSON error: %s", "nil bytes was given")
-	}
-
-	if err := os.MkdirAll(tempDir, 0777); err != nil {
-		return err
-	}
-
-	file, err := os.Create(tempDir + key)
-	if err != nil {
-		return err
-	}
-
-	_, err = file.Write(delayList)
-	if err != nil {
-		return err
-	}
-
-	if isExist := isExistTempFile(tempDir); isExist != true {
-		return fmt.Errorf("Temp file does not exist")
-	}
-
-	return nil
-}
-
-func isExistTempFile(tempFile string) bool {
-	_, err := os.Stat(tempFile)
-	return !os.IsNotExist(err)
 }
